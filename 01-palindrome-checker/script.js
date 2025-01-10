58bbdf2d563469e8aa34d7b918e9a0e2a4a1c8b3
mhdirrsyad <mohammadirsyad02@gmail.com>
2025-01-10 21:05:18 +0700

    Stack Solution

--- 01-palindrome-checker/script.js
+++ 01-palindrome-checker/script.js
@@ -2,9 +2,29 @@ const checkBtn = document.querySelector("#check-btn");
 const input = document.querySelector("#text-input");
 const result = document.querySelector("#result");
 
+// Regex
+// const checkPalindrome = (str) => {
+//   const inputStr = str.toLowerCase().replaceAll(/[-+.^:,_()\s\\/]/g, "");
+//   const inputStrRev = [...inputStr].reverse().join("");
+//   return inputStr === inputStrRev;
+// };
+
+// Stack
 const checkPalindrome = (str) => {
-  const inputStr = str.toLowerCase().replaceAll(/[-+.^:,_()\s\\/]/g, "");
-  const inputStrRev = [...inputStr].reverse().join("");
+  const stack = [];
+  const inputStr = str.toLowerCase().replace(/[-+.^:,_()\s\\/]/g, "");
+
+  // Memasukkan setiap karakter ke stack
+  for (let char of inputStr) {
+    stack.push(char);
+  }
+
+  // Reverse karakter dari stack
+  let inputStrRev = "";
+  while (stack.length > 0) {
+    inputStrRev += stack.pop();
+  }
+
   return inputStr === inputStrRev;
 };
 
